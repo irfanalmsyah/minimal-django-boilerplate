70912a9200195f2d983c8e951585e46f86da2a37
Irfan Alamsyah <88070126+irfanalmsyah@users.noreply.github.com>
2023-02-22 09:23:41 +0700

    add basic app

--- core/settings.py
+++ core/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'myFirstApp',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
